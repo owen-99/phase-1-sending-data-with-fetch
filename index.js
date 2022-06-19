999e2472fc6f57b7035b5a6c289983c97abf08be
OwenOchieng <owen.ochieng@student.moringaschool.com>
2022-06-20 01:58:39 +0300

    commit tree b70416754ffcc2bd1ccf059f5d6767b4a79db5c1

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(username, userEmail){
+    const body = document.querySelector('body');
+    const user={
+        'name': username,
+        'email': userEmail,
+    }
+    return fetch('http://localhost:3000/users',{
+        method: `POST`,
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+        body: JSON.stringify(user)
+    }).then(
+        response => response.json()
+    ).then(
+        newObj => {
+            body.innerHTML=newObj.id;
+        }
+    ).catch(
+        error => {
+            body.innerHTML='Unauthorized Access';
+        }
+    )
+}
